3812e2679bceac6ff7f87c80ddbb4c9efcfb395c
RushikeshMarathe <rushikeshmarathe784@gmail.com>
2026-01-28 16:51:52 +0530

    .Net Logger added

---
+++ FarmLink_Logger/FarmLink_Logger/Controllers/LoggerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FarmLink_Logger;
+
+namespace FarmLink.Controllers
+{
+    [ApiController]
+    [Route("logger")]
+    public class LoggerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Log([FromBody] string message)
+        {
+            FileLogger.CurrentLogger.Log(message);
+            return Ok("Logged");
+        }
+    }
+}
---
+++ FarmLink_Logger/FarmLink_Logger/Logger/FileLogger.cs
@@ -0,0 +1,33 @@
+﻿namespace FarmLink_Logger
+{
+    public class FileLogger
+    {
+        private static FileLogger _fileLogger = new FileLogger();
+
+        private FileLogger()  { }
+
+
+        public static FileLogger CurrentLogger
+        {
+            get { return _fileLogger; }
+        }
+
+
+        public void Log(string message)
+        {
+            string dir = "D:\\LoggerData";
+            string path = dir + "\\Log.txt";
+
+            Directory.CreateDirectory(dir);
+
+            using FileStream stream = new FileStream(
+                path,
+                File.Exists(path) ? FileMode.Append : FileMode.Create,
+                FileAccess.Write);
+
+            using StreamWriter writer = new StreamWriter(stream);
+            writer.WriteLine($"Logged at {DateTime.Now} - {message}");
+        }
+
+    }
+}
